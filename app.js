946df353f62329741f060edfeeaaf8a3086a751c
Drew Hart <drew@drewhart.com>
2017-03-10 15:10:37 -0600

    initial mongodb setup and wiring

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-const campgrounds = [
-  { name: 'Salmon Creek', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' },
-  { name: 'Granite Hill', image: 'https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg' },
-  { name: 'Blue Lake', image: 'https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg' },
-];
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => {
   res.render('landing');
@@ -17,7 +21,13 @@ app.get('/', (req, res) => {
 
 // Campgrounds display
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', { campgrounds });
+  Campground.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log(`error: ${err}`);
+    } else {
+      res.render('campgrounds', { campgrounds });
+    }
+  });
 });
 
 // Create a form and post to the /campgrounds post route (below)
@@ -29,10 +39,14 @@ app.get('/campgrounds/new', (req, res) => {
 app.post('/campgrounds', (req, res) => {
   const name = req.body.name;
   const image = req.body.image;
-  const newCampground = { name, image }; // using object notation shorthand
-  campgrounds.push(newCampground);
-
-  res.redirect('/campgrounds');
+  // create a new campground record in the Campground object
+  Campground.create({ name, image }, (err) => {
+    if (err) {
+      console.log(`err ${err}`);
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
 app.listen(8080, () => {
